9b2111e1a7107e88f29bcfc02199e7e4bbb9f17e
Demar-Parkes <demarparkes2750@gmail.com>
2022-11-02 20:48:43 -0400

    update file

--- wizard.py
+++ wizard.py
@@ -1,16 +1,45 @@
 import json
 
 class Player:
-    def __init__(self, name, characteristics=None, health=None, element=None, AttackLevel=None): # This function initializes the player
+    def __init__(self, name=None, characteristics=None, health=None, element=None, AttackLevel=None): # This function initializes the player
         self.name = name
         self.characteristics = characteristics
         self.health = health
         self.element = element
         self.attackLevel = AttackLevel
+        self.elements_mastered = [] # To keep track of the elements the player has mastered: At the end of the game, the length of the list must be 3.
+        self.win = 0 # Keep track of how much time the player win
+        self.lose = 0
 
+    def endGameStats(self): #we can use this function to show the player end results
+        print('###########END GAME STATS############')
+        results = {
+            'Name': self.name,
+            'Characteristics': self.characteristics,
+            'Health': self.health,
+            'Element': self.element,
+            'Element_Mastered': self.elements_mastered,
+            'Win_Count': self.win
+        }
+
+        return json.dumps(results, indent=2)
+
+    def menu
+
+
+    def element_info(self): #This function gets the user info (eg: name and characteristics)
+        content = {
+
+            'Aggressive': 'Get assigned fire with high attack power but lower life',
+            'Relaxed': 'Get water with balanced stats',
+            'Protective': 'Get high health but lower attack and life/earth element'
+
+        }
+
+        return json.dumps(content, indent=2)
 
     
-    def set_elements(self, characteristics): #We can use this function to store and set all the elements and health level: water,fire,wind,earth
+    def set_elements(self, characteristics:str): #We can use this function to store and set all the elements and health level: water,fire,wind,earth
         _elements = ['water', 'fire', 'earth', 'air']
         if characteristics == 'aggressive':
             self.element = _elements[1]
@@ -26,17 +55,22 @@ class Player:
             self.attackLevel = 50
 
 
-    def getter_setter(self): #This function can be used to set the elements and characteristics to a player
+    def getter_setter(self): #This function can be used to set the elements, characteristics and name to a player
+        get_name = input('Enter your username: ')
+        print(self.element_info())
         get_characteristic = input(f'Enter your character traits {self.character_traits()}: ')
         
         if get_characteristic.lower() in [x.lower() for x in self.character_traits()]:
-            self.characteristics = get_characteristic
+            self.characteristics = get_characteristic.lower()
             self.set_elements(self.characteristics)
+            self.name = get_name
+
         else:
             print('Enter a valid answer')
             self.getter_setter()
 
         player_construct = {
+            'Name': self.name,
             'Characteristics': f'{self.characteristics}',
             'Element': f'{self.element}',
             'Health': f'{self.health}%',
@@ -44,7 +78,6 @@ class Player:
         }
 
         return json.dumps(player_construct, indent=2)
-
 
     def character_traits(self): #We can use this function to get the characteristics of the players
         type_of_characteristics = ['Aggressive', 'Relaxed', 'Protective']
@@ -56,6 +89,5 @@ class Player:
         pass
 
 
-
-game = Player('Demar')
+game = Player()
 print(game.getter_setter())
