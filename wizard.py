6b32ab7fe7b00fad2f9f69ab188bbb1ea8730905
Demar-Parkes <demarparkes2750@gmail.com>
2022-11-05 01:57:12 -0400

    update file

--- wizard.py
+++ wizard.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 class Player:
     def __init__(self, name=None, characteristics=None, health=None, element=None, AttackLevel=None): # This function initializes the player
@@ -10,6 +11,18 @@ class Player:
         self.elements_mastered = [] # To keep track of the elements the player has mastered: At the end of the game, the length of the list must be 3.
         self.win = 0 # Keep track of how much time the player win
         self.lose = 0
+
+
+    def Credits(self): #This function print our name and show that we created the program
+        return '***Made by***\n1.Demar\n2.Ejeah\n3.David'
+
+    
+    def clear_screen(self): #This function is to just simply clear the terminal screen, you can google it: os.system('cls')
+        if os.name == 'nt':
+            os.system('cls')
+
+        elif os.name == 'posix':
+            os.system('clear')
 
     def endGameStats(self): #we can use this function to show the player end results
         print('###########END GAME STATS############')
@@ -23,8 +36,29 @@ class Player:
         }
 
         return json.dumps(results, indent=2)
+    
+    def view_player(self):
 
-    def menu
+        player_construct = {
+            'Name': self.name,
+            'Characteristics': f'{self.characteristics}',
+            'Element': f'{self.element}',
+            'Health': f'{self.health}%',
+            'Attack Level': self.attackLevel
+            }
+        
+        return json.dumps(player_construct, indent=2)
+
+    def menu(self):
+        print('1.Start Game\n2.Player Info\n3.Credits')
+        _menu_choice = input('Enter menu choice: ')
+
+        if _menu_choice == '1':
+            pass
+        elif _menu_choice == '2' and self.name != None:
+            return self.view_player()
+        elif _menu_choice == '3':
+            return self.Credits()
 
 
     def element_info(self): #This function gets the user info (eg: name and characteristics)
@@ -56,6 +90,7 @@ class Player:
 
 
     def getter_setter(self): #This function can be used to set the elements, characteristics and name to a player
+        self.clear_screen()
         get_name = input('Enter your username: ')
         print(self.element_info())
         get_characteristic = input(f'Enter your character traits {self.character_traits()}: ')
@@ -64,20 +99,12 @@ class Player:
             self.characteristics = get_characteristic.lower()
             self.set_elements(self.characteristics)
             self.name = get_name
+            return self.menu()
 
         else:
             print('Enter a valid answer')
             self.getter_setter()
 
-        player_construct = {
-            'Name': self.name,
-            'Characteristics': f'{self.characteristics}',
-            'Element': f'{self.element}',
-            'Health': f'{self.health}%',
-            'Attack Level': self.attackLevel
-        }
-
-        return json.dumps(player_construct, indent=2)
 
     def character_traits(self): #We can use this function to get the characteristics of the players
         type_of_characteristics = ['Aggressive', 'Relaxed', 'Protective']
